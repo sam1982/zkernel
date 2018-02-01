fec8389f224a13d84f2d1db6a41da9b023bf0829
root <johndoe@example.com>
2018-02-01 15:50:09 +0800

    编译脚本

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+rm -rf *.o
+rm -f kernel
 nasm -f elf32 kernel.asm -o kasm.o
+nasm -f elf32 base.asm -o base.o
+gcc -m32 -c memory.c -o memory.o
 gcc -m32 -c kernel.c -o kc.o
-ld -m elf_i386 -T link.ld -o kernel kasm.o kc.o
-qemu-system-i386 -kernel kernel
+ld -m elf_i386 -T link.ld -o kernel kasm.o kc.o memory.o base.o
+#qemu-system-i386 -kernel kernel
 
 
 
