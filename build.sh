7c7cb2af7c556f72b1ecdf3ccd48cc1a9f203f15
root <johndoe@example.com>
2018-02-03 18:11:22 +0800

    修改构建脚本，看到Makefile就烦

--- build.sh
+++ build.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 rm -rf *.o
 rm -f kernel
-nasm -f elf32 kernel.asm -o kasm.o
-nasm -f elf32 base.asm -o base.o
-gcc -m32 -c memory.c -o memory.o
-gcc -m32 -c gdt.c -o gdt.o
-gcc -m32 -c display.c -o display.o
-gcc -m32 -c kernel.c -o kc.o
-ld -m elf_i386 -T link.ld -o kernel kasm.o kc.o memory.o base.o display.o gdt.o
+for i in *.asm
+do
+    asm=$(echo $i | sed "s/.asm$//")
+    nasm -f elf32 $i -o  ${asm}.o
+done
+for i in *.c
+do
+    c=$(echo $i | sed "s/.c$//")
+    gcc -m32 -c  $i -o  ${c}.o
+done
+obj=$(ls *.o)
+ld -m elf_i386 -T link.ld -o kernel $obj
+
 #qemu-system-i386 -kernel kernel
 
 
